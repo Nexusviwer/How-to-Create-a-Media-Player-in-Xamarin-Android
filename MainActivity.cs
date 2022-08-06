1ff6086f9cdb10d676f8d3b4d8a9e62913ce007c
Lucas Gabriel <109819347+Nexusviwer@users.noreply.github.com>
2022-08-06 13:01:07 -0300

    Update MainActivity.cs

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,5 +1,8 @@
 ﻿
+//before you init code this you will need to  create a folder called raw in the resource folder app.
+//you have to add all mp3 files tha you want to play in app inside the folder raw.
 using Android.App;
+//if you dont have android.Media in your code, its just copy.
 using Android.Media;
 using Android.OS;
 using Android.Runtime;
@@ -11,7 +14,7 @@ namespace Music4u
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-
+        //here you instantiate the MediaPlayer
         MediaPlayer player1;
         MediaPlayer player2;
         MediaPlayer player3;
@@ -21,18 +24,20 @@ namespace Music4u
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-
+            //Here you have to setup de MediaPlayer
             player1 = MediaPlayer.Create(this, Resource.Raw.te_mp3);
             player2 = MediaPlayer.Create(this, Resource.Raw.ta);
 
-            //buttons take
+            //here you will configure you click event
             Button buttomPlay = FindViewById<Button>(Resource.Id.btnPlay);
             buttomPlay.Click += (sender, e) =>
             {
+            //inside your click event you have to init the player;
                 player1.Start();
                 
                 
             };
+            //Here i created this another button to pause the music.
             Button buttomPause = FindViewById<Button>(Resource.Id.btnPause);
             buttomPause.Click += (sender, e) =>
             {
